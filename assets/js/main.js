e7f1b78f310b7e62fd81f5a3253b55cf670890af
emamoldo <moldovanemanuel92.em@gmail.com>
2024-03-14 16:27:41 +0100

    Finish Milestone 1
    Fix the structure and the graphic, Add the dinamyc contact list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,55 @@
 console.log('works');
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            activeImage: 0,
+            contacts: [
+                {
+                    image: 'assets/img/avatar_1.jpg',
+                    name:'Michele',
+                    message: 'Ciao, come stai?',
+                    hour: '16:15',
+                },
+                {
+                    image: 'assets/img/avatar_2.jpg',
+                    name:'Fabio',
+                    message: 'Ciao, come stai?',
+                    hour: '16:05',
+                },
+                {
+                    image: 'assets/img/avatar_3.jpg',
+                    name:'Samuele',
+                    message: 'Ciao, come stai?',
+                    hour: '16:03',
+                },
+                {
+                    image: 'assets/img/avatar_4.jpg',
+                    name:'Alessandro B.',
+                    message: 'Ciao, come stai?',
+                    hour: '15:50',
+                },
+                {
+                    image: 'assets/img/avatar_5.jpg',
+                    name:'Alessandro L.',
+                    message: 'Ciao, come stai?',
+                    hour: '15:45',
+                },
+                {
+                    image: 'assets/img/avatar_6.jpg',
+                    name:'Giulia',
+                    message: 'Ciao, come stai?',
+                    hour: '14:30',
+                },
+                {
+                    image: 'assets/img/avatar_7.jpg',
+                    name:'Lorenzo',
+                    message: 'Ciao, come stai?',
+                    hour: '14:00',
+                },
+            ]
+        }
+    }
+}).mount('#contacts')
